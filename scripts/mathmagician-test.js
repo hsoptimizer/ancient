d623b2fe8e33f5004d800068a848038473f64b3f
hsoptimizer <hsoptimizer@users.noreply.github.com>
2015-08-22 17:00:43 +0200

    Update mathmagician-test.js

--- scripts/mathmagician-test.js
+++ scripts/mathmagician-test.js
@@ -1,10 +1,11 @@
-// version 24
+// version 29
 
 $('#savegame').keyup(import_save);
 $('body').on('change', '#laxsolo', optimize);
 $('body').on('change', '#ignoreIris', optimize);
 $('body').on('change', '#noBossLanding', optimize);
 $('body').on('change', '#primalsouls', primalSouls);
+$('body').on('change', '#theme', setTheme);
 
 var playstyle;
 var data;
@@ -17,6 +18,19 @@ $('input[type=radio][name=playstyle]').on('change', function(){ playstyle = $(th
 
 if(!Math.log10)	{
 	Math.log10 = function(t){return(Math.log(t)/Math.LN10);};
+}
+
+function setTheme()	{
+	var selectedTheme = $( "#theme option:selected" ).val();
+	saveSetting('theme', selectedTheme);
+	document.getElementById('cssId').setAttribute('href',selectedTheme+'.css');
+}
+
+function toggle_options()	{
+	var hideOptions = $('#showoptions').text() == 'Hide options';
+	$('#options').toggle();
+	$('#showoptions').text(hideOptions ? 'Show options' : 'Hide options');
+	saveSetting('hideOptions', hideOptions);
 }
 
 function plural(n, s)	{
@@ -58,6 +72,16 @@ Number.prototype.numberFormat = function(decimals, dec_point, thousands_sep) {
     parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, thousands_sep);
 
     return parts.join(dec_point);
+};
+
+String.prototype.contains = function(expression)	{
+	if(typeof expression == "string")	{
+		return(this.indexOf(expression) != -1);
+	}
+	else if(typeof expression.test == "function")	{
+		return(expression.test(this));
+	}
+	return(undefined);
 };
 
 function calcMorgulis(s)	{
@@ -115,7 +139,7 @@ anc[4] = {
 	'bonusLevel':function(lvl){return(idleBonus(lvl));},
 	'bonusDesc':'% Gold when Idle',
 	'upgradeCost':function(lvl){return(lvl);},
-	'desiredLevel':function(s){return(playstyle=='active' ? 0 : 0.93*s);}
+	'desiredLevel':function(s){return(playstyle=='active' ? 0 : 0.927*s);}
 };
 anc[5] = {
 	'Name':'Siyalatas',
@@ -139,7 +163,7 @@ anc[8] = {
 	'bonusLevel':function(lvl){return(5*lvl);},
 	'bonusDesc':'% Gold Dropped',
 	'upgradeCost':function(lvl){return(lvl);},
-	'desiredLevel':function(s){return(0.93*s);}
+	'desiredLevel':function(s){return(0.927*s);}
 };
 anc[9] = {
 	'Name':'Mimzee',
@@ -151,7 +175,7 @@ anc[9] = {
 	'bonusLevel':function(lvl){return(50*lvl);},
 	'bonusDesc':'% Treasure Chest Gold',
 	'upgradeCost':function(lvl){return(lvl);},
-	'desiredLevel':function(s){return(0.93*s);}
+	'desiredLevel':function(s){return(0.927*s);}
 };
 anc[10] = {
 	'Name':'Pluto',
@@ -283,7 +307,7 @@ anc[28] = {
 	'bonusLevel':function(lvl){return(2*lvl);},
 	'bonusDesc':'% DPS per Gild',
 	'upgradeCost':function(lvl){return(lvl);},
-	'desiredLevel':function(s){return(playstyle=='active' ? s+1 : s+10);}
+	'desiredLevel':function(s){return(playstyle=='active' ? s+1 : s+9);}
 };
 anc[29] = {
 	'Name':'Juggernaut',
@@ -295,7 +319,7 @@ anc[29] = {
 	'bonusLevel':function(lvl){return(0.01*lvl);},
 	'bonusDesc':'% Click Damage/DPS<br>(each Combo)',
 	'upgradeCost':function(lvl){return(Math.round(Math.pow(lvl, 1.5)));},
-	'desiredLevel':function(s){return(playstyle=='idle' ? 0 : playstyle=='active' ? Math.pow(s,0.8) : 0.1*s);}
+	'desiredLevel':function(s){return(playstyle=='idle' ? 0 : playstyle=='active' ? Math.pow(s,0.8) : Math.pow(0.5*s, 0.8));}
 };
 anc[30] = {
 	'Name':'Iris',
@@ -338,7 +362,7 @@ function getCookie(cname)	{
 	var ca = document.cookie.split(';');
 	for(var i=0; i<ca.length; i++) {
 		var c = ca[i].trim();
-		if (c.indexOf(name) == 0)	{
+		if (c.contains(name) == 0)	{
 			var cvalue = c.substring(name.length,c.length);
 			return(cvalue);
 		}
@@ -358,7 +382,24 @@ function setCookie(cname, cvalue, expDays) {
 }
 
 function getSetting(pname)	{
-	var value = supportsHtml5Storage() && localStorage.getItem(pname) !== null ? localStorage.getItem(pname) : getCookie(pname);
+	var value;
+	if(supportsHtml5Storage())	{
+		try	{
+		if(localStorage.getItem(pname) !== null)	{
+	// var value = supportsHtml5Storage() && localStorage.getItem(pname) !== null ? localStorage.getItem(pname) : getCookie(pname);
+			value = localStorage.getItem(pname);
+		}
+		else	{
+			value = getCookie(pname);
+		}
+		}
+		catch(e)	{
+			value = getCookie(pname);
+		}
+	}
+	else	{
+		value = getCookie(pname);
+	}
 	$('#'+pname).prop('checked', value == "true");
 	return value;
 }
@@ -381,6 +422,9 @@ function loadSettings()	{
 	playstyle = getSetting("playstyle");
 	playstyle = playstyle == "" ? 'idle' : playstyle;
 	$('input[type=radio][name=playstyle]').val([playstyle]);
+	if(getSetting('hideOptions')=='true')	{
+		toggle_options();
+	}
 }
 
 function saveSettings()	{
@@ -430,21 +474,26 @@ function loadStats(totalSouls)	{
 	var historystring = "<u>Hero Souls (end of day)</u>";
 	for(var entry in history)	{
 		var hist = history[entry];
-		var entryDate = new Date();
-		entryDate.setTime(hist.date-24*60*60*1000);
-		historystring += "<br>" + entryDate.getFullYear()+"-"+entryDate.getMonth()+"-"+entryDate.getDate() + " &mdash; " + hist.hs.numberFormat();
+		if(hist.date > lastMonth)	{
+			var entryDate = new Date();
+			entryDate.setTime(hist.date-24*60*60*1000);
+			historystring += "<br>" + entryDate.getFullYear()+"-"+entryDate.getMonth()+"-"+entryDate.getDate() + " &mdash; " + hist.hs.numberFormat();
 
-		if((hist.date <= dayLast || dayLast == today) && hist.date > yesterday)	{
-			dayLast = entry;
+			if((hist.date <= dayLast || dayLast == today) && hist.date > yesterday)	{
+				dayLast = entry;
+			}
+			if((hist.date <= ydayLast || ydayLast == today) && hist.date > dbyesterday)	{
+				ydayLast = entry;
+			}
+			if((hist.date <= weekLast || weekLast == today) && hist.date > lastWeek)	{
+				weekLast = entry;
+			}
+			if((hist.date <= monthLast || monthLast == today) && hist.date > lastMonth)	{
+				monthLast = entry;
+			}
 		}
-		if((hist.date <= ydayLast || ydayLast == today) && hist.date > dbyesterday)	{
-			ydayLast = entry;
-		}
-		if((hist.date <= weekLast || weekLast == today) && hist.date > lastWeek)	{
-			weekLast = entry;
-		}
-		if((hist.date <= monthLast || monthLast == today) && hist.date > lastMonth)	{
-			monthLast = entry;
+		else	{
+			console.log(hist.date + " is before threshold");
 		}
 	}
 
@@ -473,7 +522,7 @@ function loadStats(totalSouls)	{
 	$('#hstphour').text((hsToday / ((today-history[dayLast].date)/msPerHour)).numberFormat());
 	$('#hsyphour').text((hsYesterday / 24).numberFormat());
 	$('#hswphour').text((hsLastWeek / ((today-history[weekLast].date)/msPerHour)).numberFormat());
-	$('#hsmphour').text((hsLastMonth / ((today-history[weekLast].date)/msPerHour)).numberFormat());
+	$('#hsmphour').text((hsLastMonth / ((today-history[monthLast].date)/msPerHour)).numberFormat());
 }
 
 function saveStats(totalSouls)	{
@@ -550,8 +599,8 @@ function optimize()	{
 
 		for(key in anc)	{
 			var ancient = anc[key];
-			var optimal = ancient.desiredLevel(referenceLevel);
-			if(ancient.levelNew > 0 && (clicking == true || ancient.clicking == false) && (ignoreIris == false || key != 30))	{
+			var optimal = getOptimal(ancient, referenceLevel);
+			if(ancient.levelNew > 0 && optimal > ancient.levelNew && (clicking == true || ancient.clicking == false) && (ignoreIris == false || key != 30))	{
 				// Do not process ancients the user doesn't have.
 				// Do not process clicking ancients when clicking checkbox is off.
 				// Do not process Iris if disabled.
@@ -560,7 +609,15 @@ function optimize()	{
 
 				if(upgradeCost <= (key==5 || key==28 ? nextBank : desiredBank))	{	// always keep the desired soulbank, do not spend below this
 					// determine the ancient that is lagging behind the most (biggest relative increase from current to optimal)
-					var increase = (optimal-ancient.levelNew) / ancient.levelNew
+					var increase = (optimal-ancient.levelNew) / ancient.levelNew;
+					
+					// assign less weight to Siya/Arga to let other ancients catch up first, only upgrade when no other ancients to upgrade
+					if(playstyle == 'active' && key == 28)	{
+						increase *= 0.1;
+					}
+					else if(playstyle != 'active' && key == 5)	{
+						increase *= 0.1;
+					}
 
 					if(increase > highestIncrease)	{
 						upgradeNext = key;
@@ -603,7 +660,7 @@ function optimize()	{
 		var ancient = anc[key];
 
 		$('#new'+key).text(ancient.levelNew);
-		$('#optimal'+key).text(getOptimal(ancient, (key == 5 || key == 28) ? referenceLevel-1 : referenceLevel));
+		$('#optimal'+key).text(getOptimal(ancient, (key == 5 && playstyle!='active' || key == 28 && playstyle=='active') ? referenceLevel-1 : referenceLevel));
 
 		if(ancient.levelNew != ancient.levelOld)	{
 			$('#delta'+key).text(ancient.levelNew - ancient.levelOld);
@@ -628,8 +685,6 @@ function optimize()	{
 	saveSettings();
 }
 
-const ANTI_CHEAT_CODE = "Fe12NAfA3R6z4k0z";
-const SALT = "af0ik392jrmt0nsfdghy0";
 function import_save(evt) {
 
 	if(evt && evt.keyCode == 17)	{
@@ -640,13 +695,13 @@ function import_save(evt) {
 
 	var txt = $('#savegame').val();
 
-	if(txt.search(ANTI_CHEAT_CODE) != -1) {
-		var result = txt.split(ANTI_CHEAT_CODE);
+	if(txt.contains("Fe12NAfA3R6z4k0z")) {
+		var result = txt.split("Fe12NAfA3R6z4k0z");
 		txt = "";
 		for (var i = 0; i < result[0].length; i += 2) {
 			txt += result[0][i];
 		}
-		if (CryptoJS.MD5(txt + SALT) != result[1]) {
+		if (CryptoJS.MD5(txt + "af0ik392jrmt0nsfdghy0") != result[1]) {
 			$('#savegame').attr('class', 'error');
 			return;
 		}
@@ -672,8 +727,9 @@ function import_save(evt) {
 			var ancient = anc[key];
 
 			if(key == 0)	{
-				$('#primalsouls').prop('checked', data.heroSouls == 0);
-				ancient.levelOld = (data.heroSouls == 0 ? data.primalSouls : data.heroSouls);
+				var IncludeSoulsAfterAscension = (data.heroSouls==0 || window.location.href.contains('?primalsouls=1'));
+				$('#primalsouls').prop('checked', IncludeSoulsAfterAscension);
+				ancient.levelOld = (IncludeSoulsAfterAscension ? data.heroSouls+data.primalSouls : data.heroSouls);
 			}
 			else	{
 				if(data.ancients.ancients.hasOwnProperty(key))	{
@@ -685,7 +741,7 @@ function import_save(evt) {
 				// hide capped ancients that are already maxed but show them if they're not
 				$('#anc'+key).css('display', (ancient.maxLevel != 0 && ancient.levelOld == ancient.maxLevel) ? 'none' : 'table-row');
 			}
-			$('#old'+key).prop('value', ancient.levelOld);
+			$('#old'+key).val(ancient.levelOld);
 		}
 
 		// find the Iris bonus from relics!
@@ -711,9 +767,10 @@ function import_save(evt) {
 		$('#soulsspent').text(totalSoulsSpent.numberFormat());
 		$('#worldresets').text(data.numWorldResets.numberFormat());
 		$('#hze').text(data.highestFinishedZonePersist.numberFormat());
+		$('#titandamage').text(data.hasOwnProperty('titanDamage') ? data.titanDamage.numberFormat() : "CH v0.20+ only");
 
 		processStats(totalSoulsSpent + data.heroSouls + data.primalSouls);
-		
+		permaLink();
 		optimize();
 	}
 	else	{
@@ -721,12 +778,101 @@ function import_save(evt) {
 	}
 }
 
+function permaLink()	{
+	var url=window.location.pathname;
+	for(key in anc)	{
+		var ancient=anc[key];
+		url += "?a" + key + "=" + ancient.levelOld;
+	}
+	url+="?st="+irisBonus;
+	$('#permalink').html("<a href='"+url+"'>Permalink</a>");
+}
+
+function parseUrl()	{
+	var options = window.location.search.split("?");
+	if(options.length > 1)	{
+		var doOptimize=false;
+		for(option in options)	{
+			var arg=options[option].split("=");
+			if(arg.length==2)	{
+				var argname=arg[0];
+				var argval=parseInt(arg[1]);
+				if(!isNaN(argval))	{
+					if(/a\d+/.test(argname))	{
+						var key=parseInt(argname.substring(1));
+						if(key in anc)	{
+							var ancient=anc[key];
+							$('#old'+key).val(argval);
+							$('#anc'+key).css('display', (ancient.maxLevel != 0 && argval == ancient.maxLevel) ? 'none' : 'table-row');
+							doOptimize=true;
+						}
+					}
+					else if(/st/.test(argname))	{
+						$('#irisBonus').prop('value', argval);
+					}
+				}
+			}
+		}
+		if(doOptimize)	{
+			optimize();
+		}
+	}
+}
+
 function init()	{
 	loadSettings();
+	// findMidasLevel();
 	for(key in anc)	{
 		var ancient = anc[key];
 		if(ancient.maxLevel != 0)	{
 			$('#anc'+key).css('display', 'none');
 		}
 	}
+	document.getElementById('theme').value=getSetting('theme');
+	parseUrl();
+}
+
+function monsterHP(level)	{
+	return(10*(Math.pow(1.6, (Math.min(level,140)-1)) + Math.min(level,140)-1) * (Math.pow(1.15, Math.max(level-140,0))));
+}
+
+function monsterGold(level)	{
+	// return(monsterHP(level)/15*Math.min(3,Math.pow(1.025, Math.max(0,level-75))) * (1+anc[8].bonusLevel(anc[8].levelOld)/100));
+	return(monsterHP(level)/15*Math.min(3,Math.pow(1.025, Math.max(0,level-75))) * (1+anc[8].bonusLevel(3175)/100));
+}
+
+function findMidasLevel()	{
+// MonsterHP = 10*(1.6^(min(Level,140)-1) + min(Level,140)-1) * (1.15^max(Level-140,0))
+// MonsterGold = MonsterHP/15*min(3,1.025^max(0,Level-75)) * (1+anc[8].bonusLevel(anc[8].levelOld)/100)
+// Cid 25 + upgrades = 350+
+// Cid level cost : min(20,5+Level)*1.07^Level
+// Midas level cost: 4000000000000*1.07^Level
+// Midas upgrades: 
+
+// Level = TotalCost
+
+	var cidcost=0;
+	for(var cidlevel=1;cidlevel <= 25; cidlevel++)	{
+		cidcost += Math.min(20,5+cidlevel)*Math.pow(1.07,cidlevel);
+	}
+	
+	var midascost=0;
+	for(var midaslevel=0;midaslevel < 100;midaslevel++)	{
+		midascost += 4000000000000*Math.pow(1.07, midaslevel)*0.48;
+	}
+	
+	var upgradecost = 100+250+4e13+1e14+4e14+3.2e15+3.2e16;
+	
+	var totalcost = Math.ceil(cidcost + midascost + upgradecost);
+	
+	console.log("Cid cost: "+cidcost);
+	console.log("Midas cost: "+midascost);
+	console.log("Upgrade cost: "+upgradecost);
+	console.log("Total cost: "+totalcost);
+	
+	var level=1;
+	while(monsterGold(level) < totalcost)	{
+		level++;
+	}
+	console.log("You earn enough for Midas Start at level "+level);
 }
